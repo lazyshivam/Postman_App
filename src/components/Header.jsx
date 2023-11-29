3217d86e9b954972f4ec37e61387fca0b4c2584e
lazyshivam <91537583+lazyshivam@users.noreply.github.com>
2023-11-29 21:04:17 +0530

    update header with css m-auto in button tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -121,7 +121,7 @@ export default function Header({ user, isLoggedIn }) {
                             
                             className={classNames(
                               active ? "bg-gray-100" : "",
-                              "block  px-4 py-2 text-sm text-gray-700"
+                              "block m-auto px-4 py-2 text-sm text-gray-700"
                             )}
                           >
                             Settings
