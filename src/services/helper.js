d825730de6e9c9f1a8ca7d07b137553a2cb1dd39
lazyshivam <shivamgoswami.ss.pp@gmail.com>
2023-11-30 09:32:22 +0530

    base url issue

--- src/services/helper.js
+++ src/services/helper.js
@@ -1,3 +1,4 @@
-const BASE_URL = 'https://postman-071b.onrender.com/';
+const BASE_URL = 'https://postman-071b.onrender.com';
+// const BASE_URL = 'http://localhost:8000';
 
 export default BASE_URL;
